464a20a527711da29af841d09b0811f1d6e3c0aa
Zeriab Chiah <zeriab@Zeriabs-MBP.lan>
2021-06-20 17:37:49 +0300

    Components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
