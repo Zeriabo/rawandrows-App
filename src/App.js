fd9c3250e92827fae3ed12606400e8964c4eb0fc
Zeriab Chiah <zeriab@Zeriabs-MBP.lan>
2021-06-20 16:08:22 +0300

    enisimainen commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './Components/Header'
 function App() {
   return (
+    <header />  ,
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          get list of rows <a href="http://localhost:5000/getlistofrows">Get rows</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+
+
+
+function Header() {
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+        this is a Header
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default Header;
